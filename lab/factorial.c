f0e17baf8a7d321b88eb54afff47a7e2932adfc6
Abish-x-lethal <magarabish456@gmail.com>
2023-07-08 23:27:48 +0545

    factorial using recursion

---
+++ lab/factorial.c
@@ -0,0 +1,25 @@
+// Write a program to calculate factorial number using recursion
+
+#include <stdio.h>
+
+// Function to calculate factorial using recursion
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    int fact;
+
+    printf("Enter a positive integer: ");
+    scanf("%u", &num);
+
+    fact = factorial(num);
+    printf("Factorial of %u = %llu\n", num, fact);
+
+    return 0;
+}
